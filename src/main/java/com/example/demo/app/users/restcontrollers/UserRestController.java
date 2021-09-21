3b4b0397e0b726e4315885420207a2a2686cca89
Michelle <slavadorgamboa09@gmail.com>
2021-09-21 17:13:18 -0600

    Se agrego dependencia JPA

--- src/main/java/com/example/demo/app/users/restcontrollers/UserRestController.java
+++ src/main/java/com/example/demo/app/users/restcontrollers/UserRestController.java
@@ -1,4 +1,17 @@
 package com.example.demo.app.users.restcontrollers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserRestController {
+    @GetMapping("/{id}")
+    public Integer findById(@PathVariable("id")Integer id)
+    {
+        return id;
+    }
+    public String create(@RequestBody String usuario){
+        return usuario;
+    }
+
 }
